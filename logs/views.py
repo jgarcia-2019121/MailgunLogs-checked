ac4ebb955190ae90d11d6f3a565ba5b88e226643
jgarcia-2019121 <jgarcia-2019121@kinal.edu.gt>
2024-09-24 00:17:48 -0600

    Nuevos cambios

--- logs/views.py
+++ logs/views.py
@@ -6,21 +6,27 @@ import json
 # Función para obtener los logs con paginación y filtros
 def get_logs(request):
     # Parámetros opcionales de búsqueda
-    year = request.GET.get('year', None)
-    month = request.GET.get('month', None)
-    day = request.GET.get('day', None)
+    date = request.GET.get('date', None)
+    year, month, day = None, None, None
+    
+    if date:
+        try:
+            year, month, day = date.split('-')  # Descomponer la fecha en año, mes y día
+        except ValueError:
+            return JsonResponse({'error': 'Invalid date format'}, status=400)
+    
     event = request.GET.get('event', None)
     recipient = request.GET.get('recipient', None)
     sender_mask = request.GET.get('sender', None)
     subject = request.GET.get('subject', None)
+
     # Obtener todos los logs ordenados por fecha
-    #logs_list = Log.objects.all().order_by('-date')
     logs_list = Log.objects.filter(message__icontains="subject").order_by('-date')
 
     # Filtros de búsqueda por fecha
     if year:
         try:
-            logs_list = logs_list.filter(date__year=2000 + int(year))
+            logs_list = logs_list.filter(date__year=int(year))
         except ValueError:
             return JsonResponse({'error': 'Invalid year format'}, status=400)
     
@@ -65,41 +71,31 @@ def get_logs(request):
         sender = "N/A"
         message_data = log.message
 
-        # Log para verificar el procesamiento
-        #print(f"Processing log ID: {log.id}, message_data type: {type(message_data)}, message_data: {message_data}")
-
         # Intentar decodificar el JSON en message_data
-        message_data = log.message
         try:
-            print(message_data.replace('\'','"'))
-            #message_json = json.loads(message_data.replace('{\'','{"',1).replace('\': "{', '": "{'))
             message_json = json.loads(message_data.replace('\'','"'))
-            print(message_json)
             if isinstance(message_json, dict):
                 headers = message_json.get('headers', '{}')
             else:
                 headers = message_json
             try:
-            #headers_dict = json.loads(headers.replace('\'','"'))
                 subject = headers.get('subject', 'N/A')
                 sender = headers.get('from', 'N/A')
             except (json.JSONDecodeError, TypeError):
-                print(f"Error parsing headers for Log ID: {log.id}")
-            print(f"Log ID: {log.id}, Subject: {subject}, From: {sender}")
-        except :
-            print(f"Bad JSON: {log.id}")
-            try :
+                pass
+        except:
+            try:
                 start = message_data.index('\'subject\': ')+11
                 end = message_data.find(',', start)
                 subject = message_data[start:end].replace('\'','').replace('}"','')
-            except :
-                print(f"Subject can't be parsed: {log.id}")
-            try :
+            except:
+                pass
+            try:
                 start = message_data.index('\'from\': ')+8
                 end = message_data.find(',', start)
                 sender = message_data[start:end].replace('\'','').replace('}"','')
-            except :
-                print(f"Sender can't be parsed: {log.id}")
+            except:
+                pass
 
         # Agregar datos al log
         logs_data.append({
